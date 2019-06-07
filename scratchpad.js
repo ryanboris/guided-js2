2481aff0d53ce1675fd0e807262a24669769dc69
ryanboris <ryan.boris@gmail.com>
2019-06-06 22:07:42 -0400

    push and pop and stuff

---
+++ scratchpad.js
@@ -0,0 +1,35 @@
+//imperative --> declarative
+//how              what? (abstracted)
+
+//
+
+// const x = 3
+// // console.log(x.push('hi'))
+// console.log(arr.push('3'))
+// const y = arr.push('3')
+// console.log(y)
+
+// console.log(arr)
+// arr.push('3')
+// arr.push('3')
+// arr.push('3')
+// arr.push('3')
+// arr.push('3')
+// arr.push('3')
+// arr.push('3')
+// console.log(arr)
+//destructive or not destructive (mutate vs immutable)
+
+//.pop()
+//.unshift()
+// //.shift()
+// const cl = console.log
+// const arr = [1, 3, 45, 5, 6, 6, 3, 4, 4]
+
+// console.log('pop', arr.pop())
+// console.log('pop2', arr.pop())
+// console.log('pop3', arr.pop())
+// console.log('unshift', arr.unshift(29999))
+// console.log('shift', arr.shift())
+
+// cl('arr', arr)
